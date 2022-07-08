30d07c06f23cdf74d81522064d94cf8dfafec0af
clamp1981 <clamp1981@gmail.com>
2022-07-08 23:43:57 +0900

    feat : add EmployeeEntity.kt

---
+++ app/src/main/java/com/example/roomdemo/EmployeeEntity.kt
@@ -0,0 +1,13 @@
+package com.example.roomdemo
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName="employee-table")
+data class EmployeeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val name : String =""
+
+)
